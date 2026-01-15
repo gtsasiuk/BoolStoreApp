b9071ca1d6a5abd7af108a3fadd367f2bc2e3925
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-15 18:00:21 +0200

    Add Book entity and Enums

--- src/main/java/com/epam/rd/autocode/spring/project/model/Book.java
+++ src/main/java/com/epam/rd/autocode/spring/project/model/Book.java
@@ -1,5 +1,53 @@
 package com.epam.rd.autocode.spring.project.model;
 
+import com.epam.rd.autocode.spring.project.model.enums.AgeGroup;
+import com.epam.rd.autocode.spring.project.model.enums.Language;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Book {
-    // TODO Place your code here
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String genre;
+
+    @Column(name = "age_group", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AgeGroup ageGroup;
+
+    @Column
+    private BigDecimal price;
+
+    @Column(name = "publication_date")
+    private LocalDate publicationDate;
+
+    @Column
+    private String author;
+
+    @Column
+    private Integer pages;
+
+    @Column
+    private String characteristics;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Language language;
 }
