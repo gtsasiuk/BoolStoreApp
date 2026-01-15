d44dadf6bec5b3b0a0a1838d3c481cddb4565ef9
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-15 17:33:11 +0200

    Add Order entity

--- src/main/java/com/epam/rd/autocode/spring/project/model/Order.java
+++ src/main/java/com/epam/rd/autocode/spring/project/model/Order.java
@@ -1,5 +1,38 @@
 package com.epam.rd.autocode.spring.project.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Order {
-    // TODO Place your code here
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id", nullable = false)
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id", nullable = false)
+    private Employee employee;
+
+    @Column
+    private LocalDateTime orderDate;
+
+    @Column
+    private BigDecimal price;
+
+    @OneToMany
+    private List<BookItem> bookItems = new ArrayList<>();
 }
