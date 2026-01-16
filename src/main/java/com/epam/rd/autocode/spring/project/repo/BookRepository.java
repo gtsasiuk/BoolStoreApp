4f57b01f0691c7d07d176fd1b13808d700cadf10
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-16 22:57:39 +0200

    Updated repositories

--- src/main/java/com/epam/rd/autocode/spring/project/repo/BookRepository.java
+++ src/main/java/com/epam/rd/autocode/spring/project/repo/BookRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByName(String name);
+    boolean existsByName(String name);
 }
