2e134d5e96adfd8d2d3897fccf116a29b3e08c02
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-16 23:10:54 +0200

    Add ModelMapper and User services

--- src/main/java/com/epam/rd/autocode/spring/project/service/impl/ClientServiceImpl.java
+++ src/main/java/com/epam/rd/autocode/spring/project/service/impl/ClientServiceImpl.java
@@ -1,5 +1,59 @@
 package com.epam.rd.autocode.spring.project.service.impl;
 
-public class ClientServiceImpl{
-    //TODO Place your code here
+import com.epam.rd.autocode.spring.project.dto.ClientDTO;
+import com.epam.rd.autocode.spring.project.model.Client;
+import com.epam.rd.autocode.spring.project.repo.ClientRepository;
+import com.epam.rd.autocode.spring.project.service.ClientService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ClientServiceImpl implements ClientService {
+    private ClientRepository repository;
+    private ModelMapper mapper;
+
+    @Override
+    public ClientDTO addClient(ClientDTO client) {
+        Client newClient = mapper.map(client, Client.class);
+        Client savedClient = repository.save(newClient);
+        return mapper.map(savedClient, ClientDTO.class);
+    }
+
+    @Override
+    public List<ClientDTO> getAllClients() {
+        return repository.findAll()
+                .stream().map(client -> mapper.map(client, ClientDTO.class)).toList();
+    }
+
+    @Override
+    public ClientDTO getClientByEmail(String email) {
+        Client client = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+        return mapper.map(client, ClientDTO.class);
+    }
+
+    @Override
+    public ClientDTO updateClientByEmail(String email, ClientDTO client) {
+        Client existingClient = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+
+        existingClient.setEmail(client.getEmail());
+        existingClient.setPassword(client.getPassword());
+        existingClient.setName(client.getName());
+        existingClient.setBalance(client.getBalance());
+        Client updatedClient = repository.save(existingClient);
+
+        return mapper.map(updatedClient, ClientDTO.class);
+    }
+
+    @Override
+    public void deleteClientByEmail(String email) {
+        Client client = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+        repository.delete(client);
+    }
 }
--- src/main/java/com/epam/rd/autocode/spring/project/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/epam/rd/autocode/spring/project/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,61 @@
 package com.epam.rd.autocode.spring.project.service.impl;
 
-public class EmployeeServiceImpl{
-    //TODO Place your code here
+import com.epam.rd.autocode.spring.project.dto.EmployeeDTO;
+import com.epam.rd.autocode.spring.project.model.Employee;
+import com.epam.rd.autocode.spring.project.repo.EmployeeRepository;
+import com.epam.rd.autocode.spring.project.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository repository;
+    private ModelMapper mapper;
+
+    @Override
+    public EmployeeDTO addEmployee(EmployeeDTO employee) {
+        Employee newEmployee = mapper.map(employee, Employee.class);
+        Employee savedEmployee = repository.save(newEmployee);
+        return mapper.map(savedEmployee, EmployeeDTO.class);
+    }
+
+    @Override
+    public List<EmployeeDTO> getAllEmployees() {
+        return repository.findAll()
+                .stream().map(employee -> mapper.map(employee, EmployeeDTO.class)).toList();
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeByEmail(String email) {
+        Employee employee = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+        return mapper.map(employee, EmployeeDTO.class);
+    }
+
+    @Override
+    public EmployeeDTO updateEmployeeByEmail(String email, EmployeeDTO employee) {
+        Employee existingEmployee = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+
+        existingEmployee.setEmail(employee.getEmail());
+        existingEmployee.setPassword(employee.getPassword());
+        existingEmployee.setName(employee.getName());
+        existingEmployee.setPhone(employee.getPhone());
+        existingEmployee.setBirthDate(employee.getBirthDate());
+        Employee updatedEmployee = repository.save(existingEmployee);
+
+        return mapper.map(updatedEmployee, EmployeeDTO.class);
+    }
+
+    @Override
+    public void deleteEmployeeByEmail(String email) {
+        Employee employee = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+        repository.delete(employee);
+    }
+
 }
