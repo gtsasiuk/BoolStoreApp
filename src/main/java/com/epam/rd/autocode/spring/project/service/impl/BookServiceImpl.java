a84022a7dc821cb94bbdaad794553525a4739db4
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-17 19:19:13 +0200

    Add Order and Book services

--- src/main/java/com/epam/rd/autocode/spring/project/service/impl/BookServiceImpl.java
+++ src/main/java/com/epam/rd/autocode/spring/project/service/impl/BookServiceImpl.java
@@ -1,5 +1,65 @@
 package com.epam.rd.autocode.spring.project.service.impl;
 
-public class BookServiceImpl{
-    //TODO Place your code here
+import com.epam.rd.autocode.spring.project.dto.BookDTO;
+import com.epam.rd.autocode.spring.project.model.Book;
+import com.epam.rd.autocode.spring.project.repo.BookRepository;
+import com.epam.rd.autocode.spring.project.service.BookService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class BookServiceImpl implements BookService {
+    private BookRepository repository;
+    private ModelMapper mapper;
+
+    @Override
+    public BookDTO addBook(BookDTO book) {
+        Book newBook = mapper.map(book, Book.class);
+        Book savedBook = repository.save(newBook);
+        return mapper.map(savedBook, BookDTO.class);
+    }
+
+    @Override
+    public List<BookDTO> getAllBooks() {
+        return repository.findAll()
+                .stream().map(book -> mapper.map(book, BookDTO.class)).toList();
+    }
+
+    @Override
+    public BookDTO getBookByName(String name) {
+        Book book = repository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+        return mapper.map(book, BookDTO.class);
+    }
+
+    @Override
+    public BookDTO updateBookByName(String name, BookDTO book) {
+        Book existingBook = repository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        existingBook.setName(book.getName());
+        existingBook.setGenre(book.getGenre());
+        existingBook.setAgeGroup(book.getAgeGroup());
+        existingBook.setPrice(book.getPrice());
+        existingBook.setPublicationDate(book.getPublicationDate());
+        existingBook.setAuthor(book.getAuthor());
+        existingBook.setPages(book.getPages());
+        existingBook.setDescription(book.getDescription());
+        existingBook.setCharacteristics(book.getCharacteristics());
+        existingBook.setLanguage(book.getLanguage());
+        Book updatedBook = repository.save(existingBook);
+
+        return mapper.map(updatedBook, BookDTO.class);
+    }
+
+    @Override
+    public void deleteBookByName(String name) {
+        Book book = repository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+        repository.delete(book);
+    }
 }
--- src/main/java/com/epam/rd/autocode/spring/project/service/impl/OrderServiceImpl.java
+++ src/main/java/com/epam/rd/autocode/spring/project/service/impl/OrderServiceImpl.java
@@ -1,5 +1,98 @@
 package com.epam.rd.autocode.spring.project.service.impl;
 
-public class OrderServiceImpl{
-    //TODO Place your code here
+import com.epam.rd.autocode.spring.project.dto.BookItemDTO;
+import com.epam.rd.autocode.spring.project.dto.OrderDTO;
+import com.epam.rd.autocode.spring.project.model.*;
+import com.epam.rd.autocode.spring.project.repo.BookRepository;
+import com.epam.rd.autocode.spring.project.repo.ClientRepository;
+import com.epam.rd.autocode.spring.project.repo.EmployeeRepository;
+import com.epam.rd.autocode.spring.project.repo.OrderRepository;
+import com.epam.rd.autocode.spring.project.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private OrderRepository orderRepository;
+    private ClientRepository clientRepository;
+    private EmployeeRepository employeeRepository;
+    private BookRepository bookRepository;
+
+    private OrderDTO toDto(Order order) {
+        OrderDTO dto = new OrderDTO();
+
+        dto.setClientEmail(order.getClient().getEmail());
+        dto.setEmployeeEmail(order.getEmployee().getEmail());
+        dto.setOrderDate(order.getOrderDate());
+        dto.setPrice(order.getPrice());
+
+        List<BookItemDTO> items = order.getBookItems().stream()
+                .map(item -> new BookItemDTO(
+                        item.getBook().getName(),
+                        item.getQuantity()
+                ))
+                .toList();
+
+        dto.setBookItems(items);
+
+        return dto;
+    }
+
+    @Override
+    @Transactional
+    public OrderDTO addOrder(OrderDTO order) {
+        Client client = clientRepository.findByEmail(order.getClientEmail())
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+        Employee employee = employeeRepository.findByEmail(order.getEmployeeEmail())
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+
+        Order newOrder = new Order();
+        newOrder.setClient(client);
+        newOrder.setEmployee(employee);
+        newOrder.setPrice(order.getPrice());
+        newOrder.setOrderDate(order.getOrderDate());
+        List<BookItem> items = order.getBookItems().stream()
+                .map(itemDTO -> {
+                    Book book = bookRepository.findByName(itemDTO.getBookName())
+                            .orElseThrow(() ->
+                                    new RuntimeException("Book not found: " + itemDTO.getBookName()));
+
+                    BookItem item = new BookItem();
+                    item.setBook(book);
+                    item.setQuantity(itemDTO.getQuantity());
+                    item.setOrder(newOrder);
+
+                    return item;
+                })
+                .toList();
+        newOrder.setBookItems(items);
+
+        Order savedOrder = orderRepository.save(newOrder);
+
+        return toDto(savedOrder);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<OrderDTO> getOrdersByClient(String clientEmail) {
+        return orderRepository.findAllByClient_Email(clientEmail)
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<OrderDTO> getOrdersByEmployee(String employeeEmail) {
+        return orderRepository.findAllByEmployee_Email(employeeEmail)
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
 }
