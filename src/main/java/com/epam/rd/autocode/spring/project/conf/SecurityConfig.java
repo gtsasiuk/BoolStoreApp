66f04f6cfa7fd380a11ba3c5cb8fe27529dbeddc
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-21 23:09:34 +0200

    Add Login features to AuthController

--- src/main/java/com/epam/rd/autocode/spring/project/conf/SecurityConfig.java
+++ src/main/java/com/epam/rd/autocode/spring/project/conf/SecurityConfig.java
@@ -1,27 +1,54 @@
 package com.epam.rd.autocode.spring.project.conf;
 
+import com.epam.rd.autocode.spring.project.security.CustomUserDetailsService;
+import com.epam.rd.autocode.spring.project.security.jwt.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/", "/auth/login", "/auth/register", "/books", "/books/**", "/h2-console", "/h2-console/**").permitAll()
+                        .requestMatchers("/profile").hasAnyRole("EMPLOYEE", "CUSTOMER")
+                        .anyRequest().authenticated()
+                )
+                .userDetailsService(customUserDetailsService)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
--- src/main/java/com/epam/rd/autocode/spring/project/controller/AuthController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/AuthController.java
@@ -1,15 +1,24 @@
 package com.epam.rd.autocode.spring.project.controller;
 
+import com.epam.rd.autocode.spring.project.dto.ClientDTO;
+import com.epam.rd.autocode.spring.project.dto.auth.LoginRequestDTO;
+import com.epam.rd.autocode.spring.project.dto.auth.RegisterRequestDTO;
+import com.epam.rd.autocode.spring.project.security.CustomUserDetails;
 import com.epam.rd.autocode.spring.project.security.jwt.JwtUtil;
 import com.epam.rd.autocode.spring.project.service.ClientService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 
 
 @Controller
@@ -18,6 +27,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final ClientService clientService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
     private void addJwtCookie(HttpServletResponse response, String token) {
@@ -26,6 +36,23 @@ public class AuthController {
         cookie.setPath("/");
         cookie.setMaxAge(24 * 60 * 60);
         response.addCookie(cookie);
+    }
+
+    private void authenticate(String email, String password, HttpServletResponse response) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        String role = userDetails.getAuthorities()
+                .iterator()
+                .next()
+                .getAuthority();
+
+        String token = jwtUtil.generateToken(userDetails.getUsername(), role);
+
+        addJwtCookie(response, token);
     }
 
     @GetMapping("/login")
@@ -38,5 +65,27 @@ public class AuthController {
         return "auth/register";
     }
 
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginRequestDTO dto,
+                        HttpServletResponse response) {
+        authenticate(dto.getEmail(), dto.getPassword(), response);
+        return "redirect:/";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute RegisterRequestDTO dto,
+                           HttpServletResponse response) {
+        ClientDTO client = new ClientDTO();
+        client.setEmail(dto.getEmail());
+        client.setName(dto.getName());
+        client.setPassword(passwordEncoder.encode(dto.getPassword()));
+        client.setBalance(BigDecimal.ZERO);
+
+        clientService.addClient(client);
+
+        authenticate(dto.getEmail(), dto.getPassword(), response);
+
+        return "redirect:/";
+    }
 
 }
