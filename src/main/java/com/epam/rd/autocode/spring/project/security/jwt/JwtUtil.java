641261e120cdd700854b2184393c26ccb50440a8
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-29 17:56:27 +0200

    Change Secret and Key for JWT

--- src/main/java/com/epam/rd/autocode/spring/project/security/jwt/JwtUtil.java
+++ src/main/java/com/epam/rd/autocode/spring/project/security/jwt/JwtUtil.java
@@ -3,8 +3,9 @@ package com.epam.rd.autocode.spring.project.security.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -12,15 +13,25 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long EXPIRATION = 24 * 60 * 60 * 1000;
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    private Key key;
+
+    @PostConstruct
+    private void initKey() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public String generateToken(String email, String role) {
         return Jwts.builder()
                 .setSubject(email)
                 .claim("role", role)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(key)
                 .compact();
     }
