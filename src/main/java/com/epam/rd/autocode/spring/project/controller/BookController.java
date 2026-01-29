3779bbdc1cb4ab468461a5b97c544124e103ab84
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-01-29 17:56:59 +0200

    Add basic BookController

--- src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
@@ -1,5 +1,30 @@
 package com.epam.rd.autocode.spring.project.controller;
 
+import com.epam.rd.autocode.spring.project.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
-    // TODO Place your code here
+    private final BookService bookService;
+
+    @GetMapping
+    public String booksPage(Model model) {
+        model.addAttribute("books", bookService.getAllBooks());
+        return "books/list";
+    }
+
+    @GetMapping("/{name}")
+    public String book(@PathVariable String name, Model model) {
+        model.addAttribute("book", bookService.getBookByName(name));
+        return "books/details";
+    }
+
+
 }
