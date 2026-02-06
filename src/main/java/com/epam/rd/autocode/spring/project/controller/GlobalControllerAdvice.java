86ea72ccd406655a352ffb3a57e376392133ac11
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-02-06 21:32:56 +0200

    Add basic locale config

---
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/GlobalControllerAdvice.java
@@ -0,0 +1,14 @@
+package com.epam.rd.autocode.spring.project.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("currentUri")
+    public String currentUri(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+}
