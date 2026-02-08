7de0f818f7c102159f49fb008e38e0ed666bbbfa
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-02-08 21:13:51 +0200

    Modified validation

--- src/main/java/com/epam/rd/autocode/spring/project/dto/user/ClientDTO.java
+++ src/main/java/com/epam/rd/autocode/spring/project/dto/user/ClientDTO.java
@@ -1,5 +1,6 @@
 package com.epam.rd.autocode.spring.project.dto.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ClientDTO {
     @NotBlank
+    @Email
     private String email;
     @NotBlank
     private String password;
